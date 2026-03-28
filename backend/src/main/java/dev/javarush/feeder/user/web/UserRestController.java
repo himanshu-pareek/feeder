690f144476fa6402a89d3e5ad992fa0cbc3db454
Himanshu Pareek <himanshupareekiit01@gmail.com>
2026-03-28 10:48:58 +0530

    Add endpoint to fetch subscriptions

--- backend/src/main/java/dev/javarush/feeder/user/web/UserRestController.java
+++ backend/src/main/java/dev/javarush/feeder/user/web/UserRestController.java
@@ -1,9 +1,12 @@
 package dev.javarush.feeder.user.web;
 
+import dev.javarush.feeder.user.User;
+import dev.javarush.feeder.user.UserService;
 import dev.javarush.feeder.user.use_case.FeedSubscriptionAction;
 import dev.javarush.feeder.user.use_case.UserRegistrationAction;
 import java.net.URI;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,13 +18,21 @@ public class UserRestController {
 
   private final UserRegistrationAction userRegistrationAction;
   private final FeedSubscriptionAction subscriptionUseCase;
+  private final UserService userService;
 
   public UserRestController(
       UserRegistrationAction userRegistrationAction,
-      FeedSubscriptionAction subscriptionUseCase
+      FeedSubscriptionAction subscriptionUseCase,
+      UserService userService
   ) {
     this.userRegistrationAction = userRegistrationAction;
     this.subscriptionUseCase = subscriptionUseCase;
+    this.userService = userService;
+  }
+
+  @GetMapping
+  User get(Authentication authentication) {
+    return this.userService.getUser(authentication.getName());
   }
 
   @PostMapping("/subscribe")
